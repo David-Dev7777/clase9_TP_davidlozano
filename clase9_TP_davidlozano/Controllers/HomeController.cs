786802406d9b322b7b50f93e6fe3f3b4d240db41
Dell <Dell@DESKTOP-Q1UFDL8>
2024-12-20 20:00:31 -0300

    Agregar archivos de proyecto.

---
+++ clase9_TP_davidlozano/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using clase9_TP_davidlozano.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace clase9_TP_davidlozano.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ clase9_TP_davidlozano/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace clase9_TP_davidlozano.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
