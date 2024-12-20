3f09b8f646be8fe8d51aed542fd5bb8ba4ca7213
david-lozano <davidlozzano31@gmail.com>
2024-12-20 20:21:45 -0300

    primer commit

---
+++ clase9_TP_davidlozano/Controllers/bddController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace clase9_TP_davidlozano.Controllers
+{
+    public class bddController : Controller
+    {
+        public IActionResult ver_formulario()
+        {
+            return View("/Views/bbd/vista_bbd.cshtml");
+        }
+    }
+}
